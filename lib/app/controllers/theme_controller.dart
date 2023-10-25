53d7697b9b9e0d2d30835e68d6a4d3a405960175
nivij <nivijsuresh@gmail.com>
2023-10-25 14:07:25 +0530

    second commit

--- lib/app/controllers/theme_controller.dart
+++ lib/app/controllers/theme_controller.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import '../services/themedata.dart';
 
 class ThemeController extends GetxController {
+  final box = GetStorage();
+  static const _themeKey = 'currentTheme';
 
-  Rx<ThemeData> currentTheme = Lightmode.obs;
+  Rx<ThemeData> currentTheme = Rx<ThemeData>(Lightmode);
 
+  @override
+  void onInit() {
+    super.onInit();
+    _loadThemeFromStorage();
+  }
 
   void toggleTheme() {
     if (currentTheme.value == Lightmode) {
-      print("true");
       currentTheme.value = Darkmode;
     } else {
-      print("false");
       currentTheme.value = Lightmode;
     }
+    _saveThemeToStorage();
+  }
+
+  void _loadThemeFromStorage() {
+    final themeIndex = box.read(_themeKey);
+    if (themeIndex != null) {
+      currentTheme.value = themeIndex == 0 ? Lightmode : Darkmode;
+    }
+  }
+
+  void _saveThemeToStorage() {
+    final themeIndex = currentTheme.value == Lightmode ? 0 : 1;
+    box.write(_themeKey, themeIndex);
   }
 }

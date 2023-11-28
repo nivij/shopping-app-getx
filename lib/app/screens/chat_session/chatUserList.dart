45093a3d998d0b4266c8584aa4301113ec780a33
nivij <nivijsuresh@gmail.com>
2023-11-28 13:53:24 +0530

    third commit

--- lib/app/screens/chat_session/chatUserList.dart
+++ lib/app/screens/chat_session/chatUserList.dart
@@ -1,66 +1,56 @@
+
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:gocart/app/screens/chat_session/controller/chat_screen.dart';
 
-class ChatList extends StatefulWidget {
-  @override
-  State<ChatList> createState() => _ChatListState();
-}
+import 'controller/chat_controller.dart';
 
-class _ChatListState extends State<ChatList> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
+class ChatList extends StatelessWidget {
+  // final ChatController chatController = Get.put(ChatController());
+  final FirebaseAuth _auth =FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Customer Support'),
       ),
-      body: _buildUserList(),
+      body:_buildUserList(),
     );
   }
-
-  Widget _buildUserList() {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('users').snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          // print("Error: ${snapshot.error}");
-          return Text('Error: ${snapshot.error}');
+  Widget _buildUserList(){
+    return StreamBuilder<QuerySnapshot>(stream: FirebaseFirestore.instance.collection("users").snapshots(),
+      builder:(context, snapshot) {
+        if(snapshot.hasError){
+          return const Text('error');
         }
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          // print("Loading...");
-          return Text('Loading....');
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return const Text('loading....');
         }
-
         return ListView(
           children: snapshot.data!.docs.map<Widget>((doc) => _buildUserListItem(doc)).toList(),
         );
       },
     );
   }
-
-  Widget _buildUserListItem(DocumentSnapshot document) {
-    // print("Document: $document");
-    Map<String, dynamic>? data = document.data() as Map<String, dynamic>?;
-
-    if (data != null && data.containsKey('email') && _auth.currentUser != null && _auth.currentUser!.email != data['email']) {
-      // print("Email: ${data['email']}, UID: ${data['uid']}");
-      return ListTile(
-        title: Text(data['email'] ?? "Email not found"),
-        onTap: () {
-          // print("Tapped on email: ${data['email']}");
-          Get.to(() => ChatPage(
-            receiverUserEmail: data['email'],
-            receiverUserID: data['uid'],
-          ));
-        },
-      );
-    } else {
-      return Container();
-    }
-
+  Widget _buildUserListItem(DocumentSnapshot document){
+    Map<String,dynamic> data =document.data()! as Map<String,dynamic>;
+   if(_auth.currentUser!.email !=data ['email']){
+     return ListTile(
+       title: Text(data['email']),
+       onTap:  () {
+         Get.to(ChatPage(
+           receiverUserEmail: data['email'],
+           receiverUserID: data['uid'],));
+       },
+     );
+   }else{
+     return Container();
+   }
   }
 }
+
